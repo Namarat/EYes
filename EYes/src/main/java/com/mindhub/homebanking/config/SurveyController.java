dae50cc2391d767883ae77679b4ed67b413e7f6e
Namarat <tiaman87@gmail.com>
2022-03-23 03:24:59 -0300

    terminado el cuestionario y el diccionario, faltan las traducciones al ingles mapudungun y al creole. mejorar frontend

--- EYes/src/main/java/com/mindhub/homebanking/config/SurveyController.java
+++ EYes/src/main/java/com/mindhub/homebanking/config/SurveyController.java
@@ -26,45 +26,45 @@ public class SurveyController {
     @PostMapping("/survey/{id}")
     public ResponseEntity<Object> surveyhandler(@RequestParam("res") int res, @RequestParam("id") Long id, Model model) {
 
-    System.out.println("total:"+ res);
-    System.out.println("id: "+id);
-    ClientDTO cliente= this.clientRepository.findById(id).map(ClientDTO::new).orElse(null);
-    System.out.println("cliente: " +cliente);
+    //System.out.println("total:"+ res);
+    //System.out.println("id: "+id);
+    Client cliente= this.clientRepository.findById(id).orElse(null);
+   // System.out.println("cliente: " +cliente);
 
 
     if(res>0 && res<11){
 
         cliente.setPerfilInversion(PerfilInversionType.CONSERVADOR);
-        System.out.println("Conservador");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+       // System.out.println("Conservador");
+       // System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
     if(res>=11 && res<21){
         cliente.setPerfilInversion(PerfilInversionType.MODERADO);
-        System.out.println("Moderado");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+        //System.out.println("Moderado");
+       // System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
     if(res>=22 && res<33){
         cliente.setPerfilInversion(PerfilInversionType.CRECIMIENTO);
-        System.out.println("En crecimiento");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+       // System.out.println("En crecimiento");
+      //  System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
     if(res>=34 && res<44){
         cliente.setPerfilInversion(PerfilInversionType.DECIDIDO);
-        System.out.println("Decidido");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+      //  System.out.println("Decidido");
+      //  System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
     if(res>=45 && res<54){
         cliente.setPerfilInversion(PerfilInversionType.AUDAZ);
-        System.out.println("Audaz");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+       // System.out.println("Audaz");
+       // System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
     if(res>=55){
         cliente.setPerfilInversion(PerfilInversionType.ESPECULATIVO);
-        System.out.println("Especulativo");
-        System.out.println("perfil:"+ cliente.getPerfilInversion());
+       // System.out.println("Especulativo");
+       // System.out.println("perfil:"+ cliente.getPerfilInversion());
     }
 
-   // clientRepository.save(cliente);
+   clientRepository.save(cliente);
 
     return new ResponseEntity<>(HttpStatus.CREATED);
 }
